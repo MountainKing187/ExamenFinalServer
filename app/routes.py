54a587ab24a3bd2f262f406129f94377628e7e4a
nyest11 <nyest@aaa.com>
2025-07-20 19:39:47 -0400

    tiempo

--- app/routes.py
+++ app/routes.py
@@ -105,7 +105,7 @@ def get_ia_analisis():
     hace_un_minuto_millis = int(hace_un_minuto.timestamp() * 1000)
     
     registros = collection.find({
-        "timestamp": {
+        "Timestamp": {
             "$gte": hace_un_minuto_millis,
             "$lte": ahora_millis
         }
